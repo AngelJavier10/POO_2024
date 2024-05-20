5021040d828e50fa0b6886b647bb81d42254acb1
Angel <hangel3020@gmail.com>
2024-05-20 08:54:41 -0600

    Ejercicios

---
+++ Ejercicio_1.py
@@ -0,0 +1,12 @@
+#Crear 2 variables una pais y otra continente 
+#Mostrar su valor en pantalla
+#Poner un comentario para poner el tipo de dato
+
+pais="Mexico"
+Estado="Durango"
+
+print(f"Pais:{pais}")
+print(f"Estado:{Estado}")
+
+print(type(pais))
+print(type(Estado))
