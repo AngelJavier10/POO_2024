03d2ec77fa37e100f3203aa421131b9dc12a9b8f
Angel <hangel3020@gmail.com>
2024-05-23 09:38:27 -0600

    Ejercicios_1a6

---
+++ Parcial_1/6_Ejercicios_repaso_1al5/Ejercicio_6.py
@@ -0,0 +1,10 @@
+No_Multiplicacion=input("Seleccione la tabla de multiplicacion: Opciones 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ")
+
+if No_Multiplicacion==("1"):
+ i=1
+ num=1
+for num in range(1,10):
+  resultado=num*i
+  print(num+"X"+i)
+num+=1
+
